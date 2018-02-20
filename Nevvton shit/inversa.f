8d619c08076c421de28432c9ee68da79bb2bec6a
Nevvton <36523496+Nevvton@users.noreply.github.com>
2018-02-20 01:51:23 +0100

    Update inversa.f

--- Nevvton shit/inversa.f
+++ Nevvton shit/inversa.f
@@ -1,12 +1,12 @@
 c Programa funció inversa     ***NO FUNCIONA xdxdxd****
       program inversa
       implicit none
-      double precision yb,dy,xa,xb,xt,epsilon,error,f,y,x
+      double precision yb,dy,xa,xb,xt,epsilon,error,f,y,x,xa1,xb1
       integer n
       f(x)=dcos(x)-x-y
 c Definim tota la merda
       epsilon=1.d-10
-      error=1000.d0
+
       n=0
       open(1,file='inversa.dat')
 c Demanem que ens digui els limits de la y
@@ -26,34 +26,46 @@ c Demanem que ens digui els limits de la y
         write(*,*)'Increment massa gran'
         goto 2
       endif
-C Same with x lol
- 3    write(*,*) 'introdueix l''extrem inferior de la x'
-      read(*,*) xa
-      write(*,*) 'introdueix l''extrem superior de la x'
-      read(*,*) xb
 
-      if(xa.gt.xb) then
-        write(*,*) 'interval erroni'
-        goto 3
+C Same with x lol
+3      write(*,*) 'introdueix l''extrem inferior de la x'
+       read(*,*) xa
+       write(*,*) 'introdueix l''extrem superior de la x'
+       read(*,*) xb
+
+       if(xa.gt.xb) then
+         write(*,*) 'interval erroni'
+         goto 3
       endif
+      xa1=xa
+      xb1=xb
 c Obrim un while que actui desde el limit de l'esquerra fins arribar al de la
 c dreta amb un increment dy per cada punt.
       do while(y.lt.yb)
+        n=n+1
+        error=1000.d0
+        xa=xa1
+        xb=xb1
 c Ara un while on trobi la solucio de la funció per cada y
         do while(error.gt.epsilon)
-          n=n+1
           xt=(xa+xb)/2.d0
+          if (f(xa)*f(xb).gt.0.d0) then
+            write(*,*) 'en aquest interval no hi ha cap zero'
+            goto 3
+          endif
 
           if (f(xa)*f(xt).gt.0.d0)then
             xa=xt
           else
             xb=xt
-          endif
           error=dabs(xa-xb)
+          endif
          enddo
         write(*,*) y,xt,error,n
         write(1,*) y,xt,error,n
         y=y+dy
       enddo
+      close(1)
       stop
       end
+
